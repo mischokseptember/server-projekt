3293cb2e1c68e83e86ead87faf298cad42ff733a
Ingo Blechschmidt <iblech@speicherleck.de>
2026-03-10 07:40:48 +0100

    Weniger Einrückung für leichteres Copy&Paste

--- main.tf
+++ main.tf
@@ -63,49 +63,51 @@ resource "aws_instance" "app_server" {
   }
 
   user_data = <<-EOF
-    #!/usr/bin/env bash
+#!/usr/bin/env bash
 
-    set -ex
+echo foo > /tmp/bar
 
-    cat >> /home/ubuntu/.ssh/authorized_keys <<KEYS
-    ${join("\n", [for f in fileset("${path.module}/ssh-keys", "*.pub") : file("${path.module}/ssh-keys/${f}")])}
-    KEYS
+set -ex
 
-    # Neueste Paketdatenbank holen
-    apt update
+cat >> /home/ubuntu/.ssh/authorized_keys <<KEYS
+${join("\n", [for f in fileset("${path.module}/ssh-keys", "*.pub") : file("${path.module}/ssh-keys/${f}")])}
+KEYS
 
-    # Sicherheitshalber alle Programme auf den neuesten Stand bringen
-    apt dist-upgrade -y
+# Neueste Paketdatenbank holen
+apt update
 
-    # Wireguard installieren
-    apt install -y wireguard
+# Sicherheitshalber alle Programme auf den neuesten Stand bringen
+apt dist-upgrade -y
 
-    # Schlüsselpaar erzeugen
-    vpnpriv=$(wg genkey)
-    vpnpub=$(echo "$vpnpriv" | wg pubkey)
+# Wireguard installieren
+apt install -y wireguard
 
-    # Wireguard-Interface aktivieren
-    ip link add wg0 type wireguard
-    ip link set wg0 up
+# Schlüsselpaar erzeugen
+vpnpriv=$(wg genkey)
+vpnpub=$(echo "$vpnpriv" | wg pubkey)
 
-    # Eigene IP-Adresse innerhalb des VPN festlegen
-    ip addr add 192.168.0.1/24 dev wg0
+# Wireguard-Interface aktivieren
+ip link add wg0 type wireguard
+ip link set wg0 up
 
-    # Wireguard anweisen, auf Port 51820 auf eingehende VPN-Pakete zu lauschen
-    wg set wg0 listen-port 51820
+# Eigene IP-Adresse innerhalb des VPN festlegen
+ip addr add 192.168.0.1/24 dev wg0
 
-    # Wireguard den eigenen privaten Schlüssel bekannt machen
-    wg set wg0 private-key <(echo "$vpnpriv")
+# Wireguard anweisen, auf Port 51820 auf eingehende VPN-Pakete zu lauschen
+wg set wg0 listen-port 51820
 
-    # Wireguard einen Peer bekannt machen
-    wg set wg0 peer ${trimspace(file("vpn-keys/ingo.pub"))} allowed-ips 192.168.0.101/32 persistent-keepalive 10
-    wg set wg0 peer ${trimspace(file("vpn-keys/faruq.pub"))} allowed-ips 192.168.0.2/32 persistent-keepalive 10
-    wg set wg0 peer ${trimspace(file("vpn-keys/benni.pub"))} allowed-ips 192.168.0.69/32 persistent-keepalive 10
-    wg set wg0 peer ${trimspace(file("vpn-keys/andre.pub"))} allowed-ips 192.168.0.150/32 persistent-keepalive 10
-    wg set wg0 peer ${trimspace(file("vpn-keys/tom.pub"))} allowed-ips 192.168.0.24/32 persistent-keepalive 10
-    wg set wg0 peer ${trimspace(file("vpn-keys/denise.pub"))} allowed-ips 192.168.0.102/32 persistent-keepalive 10
- 
-    # Öffentliches Serverschloss an interessierte Handys schicken
-    curl -s -d "IP: $(curl ifconfig.me), öffentliches Schloss: $vpnpub" https://ntfy.sh/mischok-citest
-  EOF
+# Wireguard den eigenen privaten Schlüssel bekannt machen
+wg set wg0 private-key <(echo "$vpnpriv")
+
+# Wireguard einen Peer bekannt machen
+wg set wg0 peer ${trimspace(file("vpn-keys/ingo.pub"))} allowed-ips 192.168.0.101/32 persistent-keepalive 10
+wg set wg0 peer ${trimspace(file("vpn-keys/faruq.pub"))} allowed-ips 192.168.0.2/32 persistent-keepalive 10
+wg set wg0 peer ${trimspace(file("vpn-keys/benni.pub"))} allowed-ips 192.168.0.69/32 persistent-keepalive 10
+wg set wg0 peer ${trimspace(file("vpn-keys/andre.pub"))} allowed-ips 192.168.0.150/32 persistent-keepalive 10
+wg set wg0 peer ${trimspace(file("vpn-keys/tom.pub"))} allowed-ips 192.168.0.24/32 persistent-keepalive 10
+wg set wg0 peer ${trimspace(file("vpn-keys/denise.pub"))} allowed-ips 192.168.0.102/32 persistent-keepalive 10
+
+# Öffentliches Serverschloss an interessierte Handys schicken
+curl -s -d "IP: $(curl ifconfig.me), öffentliches Schloss: $vpnpub" https://ntfy.sh/mischok-citest
+EOF
 }
